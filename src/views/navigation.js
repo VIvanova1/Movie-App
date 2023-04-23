15cee66ea724b92bcaa636824aa28082215e58a7
Veneta Ivanova <i.veneta.v@gmail.com>
2023-04-23 17:16:38 +0300

    Navigation update

--- src/views/navigation.js
+++ src/views/navigation.js
@@ -3,19 +3,30 @@ import { html, render } from "../../node_modules/lit-html/lit-html.js";
 const navParent = document.querySelector('header');
 
 const navTemplate = (user) => html `
-        <nav id='menu'>
-    <input type='checkbox' id='responsive-menu'><label></label>
-    <ul>
-        <li><a href='/'>Home</a></li>
-        <li><a href='/movies'>Movies</a></li>
-        <li><a href='/serials'>serials</a></li>
-        ${user? html`
-        <li><a href='#' class="user">my list</a></li>`:
-        html `<li><a href='/profile'>profile</a></li>`}
-    </ul>
-</nav>`;
+        <nav class="menu-container">
 
-export async function updateNav(){
-const user=null;
-render(navTemplate(user), navParent)
+            <!-- burger menu -->
+            <input type="checkbox" aria-label="Toggle menu" />
+            <span></span>
+            <span></span>
+            <span></span>
+
+            <!-- logo -->
+            <a href="/" class="menu-logo"><img src="../../styles/images/movie-icon.png" alt="Movie App Icon" /></a>
+
+            <!-- menu items -->
+            <div class="menu">
+                <ul>
+                    <li><a href="/movies">Movies</a></li>
+                    <li><a href="/serials">Serials</a></li>
+                </ul>
+                <ul><li><a href="/">My favorite</a></li>
+                    <li><a href="/profile">Profile</a></li>
+                </ul>
+            </div>
+        </nav>`;
+
+export async function updateNav() {
+    const user = null;
+    render(navTemplate(user), navParent)
 }
